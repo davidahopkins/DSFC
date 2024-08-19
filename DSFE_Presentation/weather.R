acd79df099c5a389c7f57bed405a30c2965799d0
davidahopkins <davidantonhopkins@gmail.com>
2024-08-18 17:09:56 -0700

    Update weather.R

--- DSFE_Presentation/weather.R
+++ DSFE_Presentation/weather.R
@@ -14,7 +14,7 @@ library(tsibble)
 library(tsibbledata)
 library(feasts)
 library(fable)
-library(fredr)
+library(readr)
 
 #### weather data processing ####
 
@@ -26,28 +26,28 @@ ggplot(precip, aes(x = DATE, y = PRCP)) +
   labs(x = "Date", y = "Rainfall")
 
 precip$day <- format(precip$DATE, format = "%b %d")
-precip$month <- month(ymd(precip$DATE))
+precip$month <- format(as.Date(precip$DATE, format = "%y-%m-%d"), "%b")
 precip$year <- year(ymd(precip$DATE))
 head(precip)
 
 precip_m <- precip %>% 
   group_by(day) %>%
   summarize(mean_rain = mean(PRCP, na.rm = TRUE)) %>%
-  filter(mean_rain > 0.25)
+  filter(mean_rain > 0.125)
 
 ggplot(precip_m, aes(x = day, y = mean_rain)) +
   geom_bar(position="dodge", stat="identity", fill="coral2") +
   labs(x = "Date", y = "Rainfall")
 
-precip_d <- precip %>% 
+precip_avg <- precip %>% 
   filter(PRCP > 0.125) %>%
   group_by(month) %>%
   summarize(rain_days = n() / 6)
 
-ggplot(precip_d, aes(x = month, y = rain_days) +
+ggplot(precip_avg, aes(x = month, y = rain_days)) +
   geom_bar(position="dodge", stat="identity", fill="coral2") +
-  labs(x = "Date", y = "Days w/Rainfall > 0.125")
-
+  labs(x = "Date", y = "Days Rainfall > 0.125")
+  
 #### weather regression ####
 
 #### weather adjustments ####

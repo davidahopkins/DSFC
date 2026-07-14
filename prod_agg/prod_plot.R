114a383f2b7b41f3b3dc892f4988372081c2e79e
davidahopkins <davidantonhopkins@gmail.com>
2026-07-13 18:07:45 -0700

    update

--- prod_agg/prod_plot.R
+++ prod_agg/prod_plot.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(tidyverse)
 
-cash <- read_csv("Project_Cash_Flow_Data.csv")
+cash <- read_csv("Project Schedule Data.csv")
 cash$`Project Number` <- as.factor(cash$`Project Number`)
 
 ggplot(cash, aes(x = Date, y = `Cost Per Day`, color = `Project Number`)) + 

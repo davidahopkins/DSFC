380e4b4b06cc3b097ae81daa5c1658561e7f63b8
davidahopkins <davidantonhopkins@gmail.com>
2026-07-27 17:58:36 -0700

    Update est_rain.R

--- est_rain/est_rain.R
+++ est_rain/est_rain.R
@@ -49,7 +49,7 @@ rain_forecast <- forecast %>%
   mutate(int_95 = hilo(rain_days_model, 95),
          int_80 = hilo(rain_days_model, 80),
          rain_days = .mean, 
-         type = "forecast") %>%
+         type = "Forecast") %>%
   as_tsibble() %>%
   unpack_hilo(c(int_95, int_80)) %>%
   mutate(int_95_lower = pmax(0, int_95_lower),
@@ -62,36 +62,48 @@ rain_count <- rain_count %>%
          int_95_lower = rain_days,
          int_80_upper = rain_days,
          int_80_lower = rain_days, 
-         type = "historical")
+         type = "Historical")
 
 bind_rows(rain_count, rain_forecast) %>%
   ggplot(aes(x = Month, y = rain_days, fill = type)) +
   geom_col() +
-  scale_fill_manual(values = c("historical" = "purple", 
-                               "forecast" = "blue")) +
+  scale_fill_manual(values = c("Historical" = "purple", 
+                               "Forecast" = "blue")) +
   labs(title = "Rain Day Forecast", 
        x = "Date", 
        y = "Days w/Rain over 0.25 in") +
   theme(legend.title = element_blank())
 
-bind_rows(rain_count, rain_forecast) %>%
-  ggplot(aes(x = Month, y = rain_days, fill = type)) +
-  geom_ribbon(aes(ymin = int_95_lower, ymax = int_95_upper,
-                  fill = "steelblue", alpha = 0.15),
-                  show.legend = FALSE)) +
-  geom_ribbon(aes(ymin = int_80_lower, ymax = int_80_upper,
-                  fill = "steelblue", alpa = 0.25),
-                  show.legend = FALSE))+
-  geom_col() +
-  geom_errorbar(data = filter(bind_rows(rain_count, rain_forecast),
-                              data_type == "Forecast"),
-                aes(ymin = int_95_lower, ymax = int_95_upper),
-                width = 0.2, color = "darkblue", alpha = 0.7) +
-  scale_fill_manual(values = c("historical" = "purple", 
-                               "forecast" = "blue")) +
+rain_data <- bind_rows(rain_count, rain_forecast)
+
+forecast_ttls <- rain_forecast %>%
+  summarise(ttl_mean = round(sum(rain_days), 1),
+            ttl_95_up = round(sum(int_95_upper), 1),
+            ttl_95_low = round(sum(int_95_lower), 1),
+            ttl_80_up = round(sum(int_80_upper), 1),
+            ttl_80_low = round(sum(int_80_lower), 1))
+
+ggplot(rain_data, aes(x = Month, y = rain_days)) + 
+  geom_linerange(data = filter(rain_data, type == "Forecast"), 
+                 aes(ymin = int_95_lower, ymax = int_95_upper),
+                   color = "skyblue", alpha = 0.4, linewidth = 2) + 
+  geom_linerange(data = filter(rain_data, type == "Forecast"), 
+                 aes(ymin = int_80_lower, ymax = int_80_upper), 
+                     color = "steelblue", alpha = 0.5, linewidth = 2) + 
+  geom_col(aes(fill = type)) +  
+  geom_errorbar(data = filter(rain_data, type == "Forecast"),
+              aes(ymin = int_95_lower, ymax = int_95_upper), 
+              color = "darkblue", alpha = 0.8) + 
+  scale_fill_manual(values = c("Historical" = "purple", 
+                             "Forecast" = "blue")) + 
   labs(title = "Rain Day Forecast", 
-       x = "Date", 
-       y = "Days w/Rain over 0.25 in") +
-  theme(legend.title = element_blank())
+     x = "Date", 
+     y = "Days w/Rain over 0.25 in") + 
+  theme(legend.title = element_blank(),
+        panel.background = element_blank(),
+        plot.background = element_blank(),
+        axis.ticks = element_blank(),
+        panel.grid.major.y = element_line(color = "gray95"))
+
 
 
